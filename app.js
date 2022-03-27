b3f01c2f1301359e30c514aee6d62d692734a44f
Henry V. Singbeh <96744031+henrycode460@users.noreply.github.com>
2022-03-27 20:03:49 +0000

    counter with javascript

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-// gsap.fromTo('.text', {opacity:0, y:30}, {opacity:1, y:0, duration:1} )
+// set inital value to zero
+let count = 0;
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
 
-const tl = gsap.timeline({defaults: {duration:0.75, ease: "power1.out"}})
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
 
-tl.fromTo('.cokie_container', {scale: 0}, {scale:1, ease: "elastic.out(1, 0.4)", duration:1.5})
-
-tl.fromTo('.cokie', {opacity: 0, x:-40, rotation:'-45deg'}, {opacity:1, x:0, rotation:'0deg'}, '<50%')
-
-tl.fromTo('.text', {x:30, opacity:0}, {x:0, opacity:1}, '<')
-
-
-// cokie jump
-
-tl.fromTo('.cokie', {y:0, rotation:'0deg'}, {y:-20, rotation:'-20deg', yoyo:true, repeat:-1} )
-tl.fromTo('#crombs', {y:0, rotation:'0deg'}, {y:-20, rotation:'-20deg', yoyo:true, repeat:-1}, '<' )
-
-
-
-
-const button = document.querySelector('button');
- button.addEventListener('click', () =>{
-     gsap.to('.cokie_container', {opacity:0, y:100, duration: 0.75, ease:'power1.out'})
-
- })
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
+});
