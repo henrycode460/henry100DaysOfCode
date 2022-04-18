371cd92190d80783d0c845e3624044f94f3ac6d3
Henry V. Singbeh <96744031+henrycode460@users.noreply.github.com>
2022-04-18 00:13:05 +0000

    Switching Bulb

---
+++ bulbSwitch/app.js
@@ -0,0 +1,16 @@
+// var lightOn = document.getElementById("light-bulb");
+
+
+// lightOn.addEventListener("Click", function (lightOn){
+// lightOn.target.classList.toggle("bulbon");
+// })
+
+// console.log(lightOn)
+
+var lightt = document.getElementById("light-buld");
+
+lightt.addEventListener("click", function (lightt){
+    lightt.target.classList.toggle("bulbon");
+})
+
+console.log(lightt)
